79ca407727dd8d37787ce6fa67d9701ddcf75f56
Mohamed Ahmed Youssef <mohamed.jousef49@gmail.com>
2022-10-23 13:23:54 +0200

    n

--- elsewedyelectric.site/obj/Debug/net6.0/elsewedyelectric.site.GlobalUsings.g.cs
+++ elsewedyelectric.site/obj/Debug/net6.0/elsewedyelectric.site.GlobalUsings.g.cs
@@ -15,5 +15,3 @@ global using global::System.Net.Http;
 global using global::System.Net.Http.Json;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
-global using global::Umbraco.Cms.Core.DependencyInjection;
-global using global::Umbraco.Extensions;
